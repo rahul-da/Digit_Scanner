4f6f249dab709912cec690c21d5c1ccea2fcd78f
rahul-da <rahulreynoldindia@gmail.com>
2016-12-20 19:51:39 +0530

    Update regularisation_result.m

--- regularisation_result.m
+++ regularisation_result.m
@@ -1,21 +1,3 @@
-%% Machine Learning Online Class - Exercise 3 | Part 1: One-vs-all
-
-%  Instructions
-%  ------------
-% 
-%  This file contains code that helps you get started on the
-%  linear exercise. You will need to complete the following functions 
-%  in this exericse:
-%
-%     lrCostFunction.m (logistic regression cost function)
-%     oneVsAll.m
-%     predictOneVsAll.m
-%     predict.m
-%
-%  For this exercise, you will not need to change any code in this file,
-%  or any other files other than those mentioned above.
-%
-
 %% Initialization
 clear ; close all; clc
 
@@ -25,14 +7,12 @@ num_labels = 10;          % 10 labels, from 1 to 10
                           % (note that we have mapped "0" to label 10)
 
 %% =========== Part 1: Loading and Visualizing Data =============
-%  We start the exercise by first loading and visualizing the dataset. 
-%  You will be working with a dataset that contains handwritten digits.
 %
 
 % Load Training Data
 fprintf('Loading and Visualizing Data ...\n')
 
-load('ex3data1.mat'); % training data stored in arrays X, y
+load('trainingDataset.mat'); % training data stored in arrays X, y
 m = size(X, 1);
 
 % Randomly select 100 data points to display
